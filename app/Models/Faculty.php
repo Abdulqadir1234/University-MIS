cc10d550de0598473fdbc851b4ebe6bfe7066855
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:35:39 -0700

    f model created

--- app/Models/Faculty.php
+++ app/Models/Faculty.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,15 +9,13 @@ class Faculty extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'university_id', 'department_id'];
+    protected $fillable = ['name', 'university_id'];
 
     public function university() {
         return $this->belongsTo(University::class);
     }
 
-    public function department() {
-        return $this->belongsTo(Department::class);
+    public function departments() {
+        return $this->hasMany(Department::class);
     }
 }
-
-

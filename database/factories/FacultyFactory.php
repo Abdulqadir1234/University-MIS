ef2b45d1c7098c5ede0912b51e5632347d8f2507
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:36:25 -0700

    faculty factory created

--- database/factories/FacultyFactory.php
+++ database/factories/FacultyFactory.php
@@ -3,21 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Faculty;
 use App\Models\University;
 
 class FacultyFactory extends Factory
 {
-    protected $model = Faculty::class;
-
     public function definition(): array
     {
-        // Create a related university
-        $university = University::factory()->create();
-
         return [
-            'name' => $this->faker->name,
-            'university_id' => $university->id, // Assign the created university
+            'name' => $this->faker->unique()->word . ' Faculty',
+            'university_id' => University::factory(),
         ];
     }
 }

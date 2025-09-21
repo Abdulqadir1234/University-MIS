aa841fadbeb7633502082aa1e5d00088fca085aa
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:35:56 -0700

    university model editet

--- app/Models/University.php
+++ app/Models/University.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class University extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','location'];
+
+    protected $fillable = ['name'];
+
+    public function faculties() {
+        return $this->hasMany(Faculty::class);
+    }
 
     public function departments() {
         return $this->hasMany(Department::class);

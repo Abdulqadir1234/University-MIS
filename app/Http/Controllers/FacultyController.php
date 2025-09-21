b7e7d1801fae52bb66c60b85804afc9395d0fb4e
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:34:57 -0700

    created controller

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -3,52 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Faculty;
-use App\Models\Department;
 use App\Models\University;
 use Illuminate\Http\Request;
 
 class FacultyController extends Controller
 {
     public function index() {
-        // Use pagination: 10 per page
-        $faculties = Faculty::with('department.university')->paginate(10);
+        $faculties = Faculty::with('university')->paginate(10);
         return view('faculties.index', compact('faculties'));
     }
 
-public function create() {
-    $universities = University::with('departments')->get();
-    return view('faculties.create', compact('universities'));
-}
+    public function create() {
+        $universities = University::all();
+        return view('faculties.create', compact('universities'));
+    }
 
-
-   public function store(Request $request) {
-    $request->validate([
-        'name' => 'required',
-        'university_id' => 'required|exists:universities,id',
-        'department_id' => 'required|exists:departments,id',
-    ]);
-
-    Faculty::create($request->only('name','university_id','department_id'));
-
-    return redirect()->route('faculties.index')->with('success','Faculty created!');
-}
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'university_id' => 'required|exists:universities,id'
+        ]);
+        Faculty::create($request->all());
+        return redirect()->route('faculties.index')->with('success', 'Faculty created!');
+    }
 
     public function edit(Faculty $faculty) {
-        $departments = Department::with('university')->get();
-        return view('faculties.edit', compact('faculty','departments'));
+        $universities = University::all();
+        return view('faculties.edit', compact('faculty','universities'));
     }
 
     public function update(Request $request, Faculty $faculty) {
         $request->validate([
             'name' => 'required',
-            'department_id' => 'required'
+            'university_id' => 'required|exists:universities,id'
         ]);
         $faculty->update($request->all());
-        return redirect()->route('faculties.index')->with('success','Faculty updated!');
+        return redirect()->route('faculties.index')->with('success', 'Faculty updated!');
     }
 
     public function destroy(Faculty $faculty) {
         $faculty->delete();
-        return redirect()->route('faculties.index')->with('success','Faculty deleted!');
+        return redirect()->route('faculties.index')->with('success', 'Faculty deleted!');
     }
 }

700291f2fbff6431c84fc48ae0a622b64dbb4ca9
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:35:14 -0700

    un controller created

--- app/Http/Controllers/UniversityController.php
+++ app/Http/Controllers/UniversityController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 class UniversityController extends Controller
 {
     public function index() {
-        // Fetch universities with 8 per page
-        $universities = University::paginate(8);
-
+        $universities = University::paginate(10);
         return view('universities.index', compact('universities'));
     }
 
@@ -19,12 +17,9 @@ class UniversityController extends Controller
     }
 
     public function store(Request $request) {
-        $request->validate([
-            'name' => 'required'
-        ]);
-
+        $request->validate(['name' => 'required']);
         University::create($request->all());
-        return redirect()->route('universities.index')->with('success','University created!');
+        return redirect()->route('universities.index')->with('success', 'University created!');
     }
 
     public function edit(University $university) {
@@ -32,16 +27,13 @@ class UniversityController extends Controller
     }
 
     public function update(Request $request, University $university) {
-        $request->validate([
-            'name' => 'required'
-        ]);
-
+        $request->validate(['name' => 'required']);
         $university->update($request->all());
-        return redirect()->route('universities.index')->with('success','University updated!');
+        return redirect()->route('universities.index')->with('success', 'University updated!');
     }
 
     public function destroy(University $university) {
         $university->delete();
-        return redirect()->route('universities.index')->with('success','University deleted!');
+        return redirect()->route('universities.index')->with('success', 'University deleted!');
     }
 }

2b984204334eda054217d906d8630d700debbd60
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-21 04:36:10 -0700

    dep factory created

--- database/factories/DepartmentFactory.php
+++ database/factories/DepartmentFactory.php
@@ -3,23 +3,17 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Department;
-use App\Models\Faculty;
 use App\Models\University;
+use App\Models\Faculty;
 
 class DepartmentFactory extends Factory
 {
-    protected $model = Department::class;
-
     public function definition(): array
     {
-        // Create or get a faculty with its university
-        $faculty = Faculty::factory()->create();
-
         return [
             'name' => $this->faker->unique()->word . ' Department',
-            'faculty_id' => $faculty->id,
-            'university_id' => $faculty->university_id, // Match university to faculty
+            'university_id' => University::factory(),
+            'faculty_id' => Faculty::factory(),
         ];
     }
 }

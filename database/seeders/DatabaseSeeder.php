b053902391658da460719fe7c5e15bf0b86c52b0
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-23 02:13:55 -0700

    edited the code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,29 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\University;
+use App\Models\Faculty;
+use App\Models\Department;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
+    public function run()
     {
-        $this->call([
-            UniversitySeeder::class,
-            FacultySeeder::class,
-            DepartmentSeeder::class,
-        ]);
+        // Create 3 universities
+        University::factory(3)->create()->each(function ($university) {
+
+            // Each university has 2-4 faculties
+            $faculties = Faculty::factory(rand(2, 4))->create([
+                'university_id' => $university->id,
+            ]);
+
+            // Each faculty has 2-5 departments
+            $faculties->each(function ($faculty) use ($university) {
+                Department::factory(rand(2,5))->create([
+                    'university_id' => $university->id,
+                    'faculty_id' => $faculty->id,
+                ]);
+            });
+        });
     }
 }

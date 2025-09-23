4877fcbc41fb074fb60c8aed0093a5a20b78410e
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-23 02:12:57 -0700

    edited the code

--- database/factories/DepartmentFactory.php
+++ database/factories/DepartmentFactory.php
@@ -3,16 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\University;
+use App\Models\Department;
 use App\Models\Faculty;
 
 class DepartmentFactory extends Factory
 {
-    public function definition(): array
+    protected $model = Department::class;
+
+    public function definition()
     {
         return [
-            'name' => $this->faker->unique()->word . ' Department',
-            'university_id' => University::factory(),
+            'name' => $this->faker->word . ' Department',
+            'university_id' => Faculty::factory()->create()->university_id,
             'faculty_id' => Faculty::factory(),
         ];
     }

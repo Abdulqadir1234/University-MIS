10ac36b67025553717dca7ee26b265f7fba21e64
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-09-23 02:12:13 -0700

    edited the code

--- app/Http/Controllers/UniversityController.php
+++ app/Http/Controllers/UniversityController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\University;
@@ -7,33 +6,35 @@ use Illuminate\Http\Request;
 
 class UniversityController extends Controller
 {
+    // Show universities with pagination, newest first
     public function index() {
-        $universities = University::paginate(10);
+        $universities = University::orderBy('id', 'desc') // newest first
+            ->paginate(10); // 10 per page
         return view('universities.index', compact('universities'));
     }
 
-    public function create() {
-        return view('universities.create');
+    public function create() { 
+        return view('universities.create'); 
     }
 
     public function store(Request $request) {
-        $request->validate(['name' => 'required']);
+        $request->validate(['name'=>'required']);
         University::create($request->all());
-        return redirect()->route('universities.index')->with('success', 'University created!');
+        return redirect()->route('universities.index');
     }
 
-    public function edit(University $university) {
-        return view('universities.edit', compact('university'));
+    public function edit(University $university) { 
+        return view('universities.edit', compact('university')); 
     }
 
     public function update(Request $request, University $university) {
-        $request->validate(['name' => 'required']);
+        $request->validate(['name'=>'required']);
         $university->update($request->all());
-        return redirect()->route('universities.index')->with('success', 'University updated!');
+        return redirect()->route('universities.index');
     }
 
-    public function destroy(University $university) {
-        $university->delete();
-        return redirect()->route('universities.index')->with('success', 'University deleted!');
+    public function destroy(University $university) { 
+        $university->delete(); 
+        return redirect()->route('universities.index'); 
     }
 }

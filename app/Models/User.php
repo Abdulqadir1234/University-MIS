be9b190c1d913e926b2ef1830fe232af5c932085
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-10-06 03:42:21 -0700

    updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function courses()
+{
+    return $this->belongsToMany(\App\Models\Course::class);
+}
+
 }

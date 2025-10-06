030d5ca0873cbe2ecddce83ea303bbbb8eb532d5
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-10-06 03:43:35 -0700

    created

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dashboard'     => 'Dashboard',
+    'universities'  => 'Universities',
+    'faculties'     => 'Faculties',
+    'departments'   => 'Departments',
+    'logout'        => 'Logout',
+    'login'         => 'Login',
+    'register'      => 'Register',
+    'english'       => 'English',
+    'persian'       => 'فارسی',
+];

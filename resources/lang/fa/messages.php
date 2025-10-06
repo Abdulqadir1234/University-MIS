599b1db30649aac0d027095d08fcc8200f08b9c3
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-10-06 03:43:41 -0700

    created

---
+++ resources/lang/fa/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dashboard'     => 'داشبورد',
+    'universities'  => 'دانشگاه‌ها',
+    'faculties'     => 'دانشکده‌ها',
+    'departments'   => 'دپارتمان‌ها',
+    'logout'        => 'خروج',
+    'login'         => 'ورود',
+    'register'      => 'ثبت‌نام',
+    'english'       => 'English',
+    'persian'       => 'فارسی',
+];

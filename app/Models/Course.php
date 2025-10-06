a56b533ec6fba535fd446fa864909489b5be600c
Abdulqadir1234 <abdulqadirnasrat@gmail.com>
2025-10-06 03:42:11 -0700

    model created for courses

---
+++ app/Models/Course.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'faculty_id',
+        'department_id',
+        'credit_hours',
+    ];
+
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+    public function teachers()
+{
+    return $this->belongsToMany(\App\Models\User::class);
+}
+
+}
